a7aa30f9375691214d37a15641c6644e25553729
Alex Tsapkov <alexiscom@icloud.com>
2019-06-17 01:53:47 +0300

    sort fix

--- Inf/9/key_sort.c
+++ Inf/9/key_sort.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 
+
+int is_num(char c)
+{
+  return (c >= '0' && c <= '9') || c == '.' ? 1 : 0;
+}
+int is_space(char c)
+{
+  return c == '\n' || c == '\t' || c == ' ' ? 1 : 0;
+}
 
 void siftDown(float *numbers, size_t *values, int root, int bottom)
 {
@@ -52,49 +62,57 @@ int main(void)
     float keys[N];
     size_t vals[N];
 
-    
     for(int i = 0; i < N; i++) {
-        puts("Ue");
         vals[i] = (size_t)malloc(sizeof(char) * 100);
         scanf("%f %s",&(keys[i]), (char*)vals[i]);
-        puts("rr");
     }
     puts("Unsorted:");
     for(int i = 0; i < N; i++) {
         printf("%f %s\n",keys[i], (char*)vals[i]);
     }
+    long start = clock();
     heapSort(keys, vals, N);
     puts("");
     puts("Sorted:");
     for(int i = 0; i < N; i++) {
         printf("%f %s\n",keys[i], (char*)vals[i]);
     }
-    
+    printf("%ld\n", clock() - start);
+    getchar();
     float key;
-    while(scanf("%f", &key)) {
-        puts("Enter:");
-        int a = 0;
-        int b = N - 1;
-        while(abs(a - b) > 1) {
-            if (key > keys[(a + b) / 2]) {
-                a = (a + b) / 2;
-            } else if (key < keys[(a + b) / 2]) {
-                b = (a + b) / 2;
-            } else {
-                a = (a + b) / 2;
-                b = a;
-            }
+    char c;
+    int i = 0;
+    char tmp_str[20];
+    while((c = getchar()) != EOF) {
+        if(is_num(c)) {
+          tmp_str[i] = c;
+          i++;
+        } else if(is_space(c)) {
+          tmp_str[i] = '\0';
+          i = 0;
+          key = atof(tmp_str);
+          puts("Enter:");
+          int a = 0;
+          int b = N - 1;
+          while(abs(a - b) > 1) {
+              if (key > keys[(a + b) / 2]) {
+                  a = (a + b) / 2;
+              } else if (key < keys[(a + b) / 2]) {
+                  b = (a + b) / 2;
+              } else {
+                  a = (a + b) / 2;
+                  b = a;
+              }
+          }
+          if (a != b) {
+              if (fabsf(key - keys[a]) < fabsf(key - keys[b])) {
+                  b = a;
+              } else {
+                  a = b;
+              }
+          }
+          printf("%f %s\n",keys[a], (char*)vals[a]);
         }
-        if (a != b) {
-            if (fabsf(key - keys[a]) < fabsf(key - keys[b])) {
-                b = a;
-            } else {
-                a = b;
-            }
-        }
-        printf("%f %s\n",keys[a], (char*)vals[a]);
-
     }
-    
     return 0;
 }
